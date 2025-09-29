350b9c63d3ec0053dafa16cec04fd77f28cbb9ce
Ludovic Delhomme <74348195+Datayama38@users.noreply.github.com>
2025-09-29 17:39:44 +0200

    fix: remove last value in charts (#2960)

--- public/src/app/observatoire/territoire/graphs/FluxGraph.tsx
+++ public/src/app/observatoire/territoire/graphs/FluxGraph.tsx
@@ -1,9 +1,9 @@
-import DownloadButton from '@/components/observatoire/DownloadButton';
-import { GetApiUrl } from '@/helpers/api';
-import { chartLabels } from '@/helpers/graph';
-import { useApi } from '@/hooks/useApi';
-import { FluxIndicators } from '@/interfaces/observatoire/componentsInterfaces';
-import { fr } from '@codegouvfr/react-dsfr';
+import DownloadButton from "@/components/observatoire/DownloadButton";
+import { GetApiUrl } from "@/helpers/api";
+import { chartLabels } from "@/helpers/graph";
+import { useApi } from "@/hooks/useApi";
+import { FluxIndicators } from "@/interfaces/observatoire/componentsInterfaces";
+import { fr } from "@codegouvfr/react-dsfr";
 import {
   CategoryScale,
   Chart as ChartJS,
@@ -14,13 +14,28 @@ import {
   PointElement,
   Title,
   Tooltip,
-} from 'chart.js';
-import { Line } from 'react-chartjs-2';
-import { useDashboardContext } from '../../../../context/DashboardProvider';
+} from "chart.js";
+import { Line } from "react-chartjs-2";
+import { useDashboardContext } from "../../../../context/DashboardProvider";
 
-ChartJS.register(CategoryScale, LinearScale, PointElement, LineElement, Title, Tooltip, Legend, Filler);
+ChartJS.register(
+  CategoryScale,
+  LinearScale,
+  PointElement,
+  LineElement,
+  Title,
+  Tooltip,
+  Legend,
+  Filler
+);
 
-export default function TrajetsGraph({ title,indic }: { title: string, indic:FluxIndicators }) {
+export default function TrajetsGraph({
+  title,
+  indic,
+}: {
+  title: string;
+  indic: FluxIndicators;
+}) {
   const { dashboard } = useDashboardContext();
   const options = {
     responsive: true,
@@ -34,67 +49,76 @@ export default function TrajetsGraph({ title,indic }: { title: string, indic:Flu
     `indic=${indic}`,
     `code=${dashboard.params.code}`,
     `type=${dashboard.params.type}`,
-    `year=${dashboard.params.year}`
+    `year=${dashboard.params.year}`,
   ];
-  const url = GetApiUrl('evol-flux', params);
+  const url = GetApiUrl("evol-flux", params);
   const { data, error, loading } = useApi<Record<string, number>[]>(url);
-  const dataset = data?.map((d) => d[`${indic}`]).reverse();
+  const dataset = data
+    ?.map((d) => d[`${indic}`])
+    .reverse()
+    .slice(0, -1);
   const datasets = [
     {
       data: dataset,
       fill: true,
-      borderColor: '#000091',
-      backgroundColor: 'rgba(0, 0, 145, 0.2)',
+      borderColor: "#000091",
+      backgroundColor: "rgba(0, 0, 145, 0.2)",
       tension: 0.1,
     },
   ];
-  const chartData = { labels: chartLabels(data ? data : [] , dashboard.params.period) , datasets: datasets };
+  const chartData = {
+    labels: chartLabels(data ? data : [], dashboard.params.period).slice(0, -1),
+    datasets: datasets,
+  };
   return (
     <>
       {loading && (
-        <div className={fr.cx('fr-callout')}>
-          <h3 className={fr.cx('fr-callout__title')}>{title}</h3>
+        <div className={fr.cx("fr-callout")}>
+          <h3 className={fr.cx("fr-callout__title")}>{title}</h3>
           <div>Chargement en cours...</div>
         </div>
       )}
       {error && (
-        <div className={fr.cx('fr-callout')}>
-          <h3 className={fr.cx('fr-callout__title')}>{title}</h3>
+        <div className={fr.cx("fr-callout")}>
+          <h3 className={fr.cx("fr-callout__title")}>{title}</h3>
           <div>{`Un problème est survenu au chargement des données: ${error}`}</div>
         </div>
       )}
-      {!data || data.length == 0 && (
-        <div className={fr.cx('fr-callout')}>
-          <h3 className={fr.cx('fr-callout__title')}>{title}</h3>
-          <div>Pas de données disponibles pour ce graphique...</div>
-        </div>
-      )}
+      {!data ||
+        (data.length == 0 && (
+          <div className={fr.cx("fr-callout")}>
+            <h3 className={fr.cx("fr-callout__title")}>{title}</h3>
+            <div>Pas de données disponibles pour ce graphique...</div>
+          </div>
+        ))}
       {!loading && !error && data && data.length > 0 && (
-        <div className={fr.cx('fr-callout')}>
-          <div className={fr.cx('fr-callout__title','fr-text--xl')}>
-            {title}  
-            <span className={fr.cx('fr-pl-5v')}>
-              <DownloadButton 
-                title={'Télécharger les données du graphique'}
+        <div className={fr.cx("fr-callout")}>
+          <div className={fr.cx("fr-callout__title", "fr-text--xl")}>
+            {title}
+            <span className={fr.cx("fr-pl-5v")}>
+              <DownloadButton
+                title={"Télécharger les données du graphique"}
                 data={data!}
-                filename='flux'
+                filename="flux"
               />
             </span>
           </div>
-          <figure className='graph-wrapper' style={{ backgroundColor: '#fff' }}>
+          <figure className="graph-wrapper" style={{ backgroundColor: "#fff" }}>
             <Line options={options} data={chartData} aria-hidden />
-            { dataset &&
-              <figcaption className={fr.cx('fr-sr-only')}>
+            {dataset && (
+              <figcaption className={fr.cx("fr-sr-only")}>
                 <ul>
-                  { dataset.map((d,i) =>{
+                  {dataset.map((d, i) => {
                     return (
-                      <li key={i}>{chartData.labels[i]} : {d} trajets</li>
-                    )
-                  })} 
-                </ul> 
+                      <li key={i}>
+                        {chartData.labels[i]} : {d} trajets
+                      </li>
+                    );
+                  })}
+                </ul>
               </figcaption>
-            }
-          </figure> 
+            )}
+          </figure>
         </div>
       )}
     </>
