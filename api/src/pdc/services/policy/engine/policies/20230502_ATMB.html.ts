91d4ab04b866e17cc7606fdcd7455e002e1f6d26
Jonathan Fallon <jonathan.fallon@beta.gouv.fr>
2025-11-05 22:49:29 +0100

    add ATMB 2025 (#2975)

--- api/src/pdc/services/policy/engine/policies/20230502_ATMB.html.ts
+++ api/src/pdc/services/policy/engine/policies/20230502_ATMB.html.ts
@@ -1,6 +1,6 @@
 export const description = `
 <div _ngcontent-pmm-c231="" id="summary" class="campaignSummaryText-content-text">
-  <p>Campagne d'incitation au covoiturage du <b>2 mai 2023 au 31 décembre 2025</b>, toute la semaine</p>
+  <p>Campagne d'incitation au covoiturage du <b>2 mai 2023 au 13 octobre 2025</b>, toute la semaine</p>
   <p>Cette campagne est limitée à <b>105 000,00 €</b>.</p>
   <p>Le périmètre géographique de la campagne comprend les zones suivantes :</p>
   <ul>
---
+++ api/src/pdc/services/policy/engine/policies/20251014_ATMB.html.ts
@@ -0,0 +1,39 @@
+export const description = `
+<div _ngcontent-pmm-c231="" id="summary" class="campaignSummaryText-content-text">
+
+  <p>Campagne d'incitation au covoiturage du <b>14 octobre 2025 au 31 décembre 2025</b>, toute la semaine</p>
+
+  <p>Cette campagne est limitée à <b>8 000,00 €</b>.</p>
+
+  <p>Le périmètre géographique de la campagne comprend les zones suivantes :</p>
+
+  <ul>
+    <li>Communauté de communes Cluses Arve et Montagnes</li>
+    <li>Communauté de communes Pays du Mont-Blanc</li>
+    <li>Communauté de communes Vallée de Chamonix Mont-Blanc</li>
+    <li>Communauté de communes des Montagnes du Giffre</li>
+  </ul>
+
+  <p>
+    Les <b>conducteurs</b> effectuant un trajet d'au moins 2 km,
+    avec pour origine et destination le périmètre ci-dessus,
+    sont incités selon les règles suivantes :
+  </p>
+
+  <ul>
+    <li><b>De 4 à 10 km : 1,50 € par trajet par passager transporté.</b></li>
+    <li><b>De 10 à 25 km : -0,10 € par km par passager</b></li>
+  </ul>
+
+  <p>Les restrictions suivantes seront appliquées :</p>
+
+  <ul>
+    <li>50,00 € maximum pour le conducteur par mois.</li>
+  </ul>
+
+  <p>
+    La campagne est limitée aux opérateurs <b>Karos et BlablaCar Daily</b>
+    proposant des preuves de classe <b>B ou C</b>.
+  </p>
+</div>
+`;
