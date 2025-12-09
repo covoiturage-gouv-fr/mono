63497a2e13515c4920df2e75f6337067e31b6f21
Jonathan Fallon <jonathan@scopopop.com>
2025-12-09 10:15:11 +0100

    install zizmor in nix-shell

--- flake.nix
+++ flake.nix
@@ -63,6 +63,7 @@
               # misc
               gh
               yq-go
+              zizmor
             ];
             shellHook = ''
               export PATH="$PWD/node_modules/.bin/:$PATH"
