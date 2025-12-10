430ff02046b75ef35e5ea56ee45d39f48838ca5a
Jonathan Fallon <jonathan@scopopop.com>
2025-12-10 18:45:15 +0100

    add password support to mock login

--- api/src/pdc/services/auth/config/index.ts
+++ api/src/pdc/services/auth/config/index.ts
@@ -5,6 +5,7 @@ import * as dex from "./dex.ts";
 import * as jwt from "./jwt.ts";
 import * as permissions from "./permissions.ts";
 import * as proconnect from "./proconnect.ts";
+import * as test from "./test.ts";
 
 export const env = env_or_fail("NODE_ENV", "local");
 export const app_url = env_or_fail("APP_DASHBOARD_V2_URL");
@@ -17,4 +18,5 @@ export const config = {
   permissions,
   proconnect,
   proxy,
+  test,
 };
