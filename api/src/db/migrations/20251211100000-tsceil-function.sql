c09b72e055c97deef16915695dab84c8f92d3e8a
Jonathan Fallon <jonathan@scopopop.com>
2025-12-11 16:54:25 +0100

    rm ts_ceil migration

--- api/src/db/migrations/20251211100000-tsceil-function.sql
+++
@@ -1,6 +0,0 @@
-CREATE OR REPLACE FUNCTION ts_ceil(ts timestamp, step integer)
-  RETURNS timestamp AS $$
-  BEGIN
-    RETURN to_timestamp(ceil(extract(epoch from ts) / step) * step);
-  END;
-$$ LANGUAGE plpgsql IMMUTABLE;
