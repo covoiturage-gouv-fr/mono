1ecfcec9e95483f8853eca02b513ad1e73f47f92
Jonathan Fallon <jonathan@scopopop.com>
2026-01-27 12:56:16 +0100

    add covoit_idfm_2026 campaign configuration

---
+++ api/src/pdc/services/policy/engine/policies/20251201_Covoit_IDFM_2026.ts
@@ -0,0 +1,185 @@
+import { RunnableSlices } from "../../interfaces/engine/PolicyInterface.ts";
+import {
+  OperatorsEnum,
+  PolicyHandlerInterface,
+  PolicyHandlerParamsInterface,
+  PolicyHandlerStaticInterface,
+  StatelessContextInterface,
+} from "../../interfaces/index.ts";
+import { NotEligibleTargetException } from "../exceptions/NotEligibleTargetException.ts";
+import { atDate } from "../helpers/atDate.ts";
+import { getOperatorsAt, TimestampedOperators } from "../helpers/getOperatorsAt.ts";
+import { isAfter } from "../helpers/isAfter.ts";
+import { isBefore } from "../helpers/isBefore.ts";
+import { isOperatorClassOrThrow } from "../helpers/isOperatorClassOrThrow.ts";
+import { isOperatorOrThrow } from "../helpers/isOperatorOrThrow.ts";
+import {
+  LimitTargetEnum,
+  watchForGlobalMaxAmount,
+  watchForMaxPassengersPerDriverPerDay,
+  watchForMaxPassengersPerTrip,
+  watchForPassengerMaxByTripByDay,
+  watchForPersonMaxAmountByMonth,
+  watchForPersonMaxTripByDay,
+} from "../helpers/limits.ts";
+import { onDistanceRange, onDistanceRangeOrThrow } from "../helpers/onDistanceRange.ts";
+import { perKm, perSeat } from "../helpers/per.ts";
+import { endsAt, startsAt, startsOrEndsAt } from "../helpers/position.ts";
+import { description } from "./20210520_IDFM.html.ts";
+import { AbstractPolicyHandler } from "./AbstractPolicyHandler.ts";
+
+// Article 5 TRAJETS ÉLIGIBLES
+
+// Conformément aux exigences de l’article 6.2.1 du Cahier des Clauses Techniques Particulières (CCTP) du marché 2024-031, le soutien alloué par Île-de-France Mobilités à la pratique du covoiturage sera limité intrinsèquement aux trajets :
+//
+//   • De plus de 2 kilomètres, dont l’origine et la destination sont sur le territoire francilien et dont l’origine ou
+//     la destination sont situées hors de la commune de Paris.
+//   • Effectués par des conducteurs et par des passagers majeurs, pouvant être domiciliés à la même adresse.
+//   • Réalisés par le biais de l’Application Covoit IDFM déployée pour le service Covoiturage Île-de-France Mobilités.
+//   • Inscrits dans le Registre de Preuve de Covoiturage (RPC) avec un niveau de classe de type C tel que défini par le
+//     RPC.
+//
+// Pour être éligible, nous nous assurerons qu’un trajet covoituré respectera l’ensemble des critères de l'article 6.2.2
+// du CCTP Marché 2024_031), listés ci-dessous :
+//
+//   • Règles de plafonnement : Nous appliquerons un plafonnement de soutien financier d’Île de-France Mobilités à 200 €
+//     par mois et par conducteur. Chaque trajet aura un maximum de 3 passagers. Un conducteur est limité à un maximum
+//     de 6 passagers transportés par jour. Quant au nombre de trajets maximum par jour, ils sont plafonnés à 4 trajets
+//     par utilisateur par jour (indépendamment du nombre de passagers par trajet).
+//   • Non-tronçonnage des trajets : un covoitureur réalise deux trajets consécutivement un même jour, le second trajet
+//     ne sera pas éligible s’il a débuté moins d’une demi-heure après la fin du précédent.
+//   • Non-respect des modalités d'utilisation de l’Application : les utilisateurs s’engagent à respecter l’ensemble des
+//     CGUs qu’ils ont signées lors de leur inscription.
+
+// Campagne d'Île-de-France Mobilité
+export const CovoitIDFM2026: PolicyHandlerStaticInterface = class extends AbstractPolicyHandler
+  implements PolicyHandlerInterface {
+  static readonly id = "covoit_idfm_2026";
+
+  constructor(public max_amount: bigint) {
+    super();
+    this.limits = [
+      // Nous appliquerons un plafonnement de soutien financier d’IDFM à 200 € par mois et par conducteur.
+      [
+        "dcee0b35-222c-46f1-aa49-cb76725aabeb",
+        200_00,
+        watchForPersonMaxAmountByMonth,
+        LimitTargetEnum.Driver,
+      ],
+
+      // Le nombre de trajets maximum par jour, ils sont plafonnés à 4 trajets par utilisateur par jour
+      // (indépendamment du nombre de passagers par trajet).
+      [
+        "13b6f5ac-2764-4865-9224-3f49989a3734",
+        4,
+        watchForPersonMaxTripByDay,
+        LimitTargetEnum.Driver,
+      ],
+
+      // Chaque trajet aura un maximum de 3 passagers
+      [
+        "72361283-eec4-44a6-9f83-8953281f9627",
+        3,
+        watchForMaxPassengersPerTrip,
+      ],
+
+      // Un conducteur est limité à un maximum de 6 passagers transportés par jour
+      [
+        "d55bc553-7841-4e29-82f6-0505b9efaef6",
+        6,
+        watchForMaxPassengersPerDriverPerDay,
+      ],
+
+      // Plafonnement de la campagne (16M€)
+      [
+        "e1771f85-8ec1-4d60-bf42-a3515e2ad9dd",
+        max_amount,
+        watchForGlobalMaxAmount,
+      ],
+    ];
+  }
+
+  protected operators: TimestampedOperators = [
+    // Réalisés par le biais de l’Application Covoit IDFM
+    {
+      date: new Date("2025-12-01T00:00:00+0100"),
+      operators: [
+        OperatorsEnum.COVOIT_IDFM,
+      ],
+    },
+  ];
+
+  // Article 6 Montant de la campagne
+  //
+  //  • Prix passager : 0€ de 2 à 80km
+  //  • Incitation conducteur : 2€ de 2 à 20km puis 0,10€ par km / supplémentaire (capé à 3€ par trajet et par passager)
+
+  protected slices: RunnableSlices = [
+    {
+      start: 2_000,
+      end: 20_000,
+      fn: (ctx: StatelessContextInterface) => perSeat(ctx, 200),
+    },
+    {
+      start: 20_000,
+      // end: 80_000,
+      fn: (ctx: StatelessContextInterface) => perSeat(ctx, perKm(ctx, { amount: 10, offset: 20_000, limit: 30_000 })),
+    },
+  ];
+
+  protected processExclusion(ctx: StatelessContextInterface) {
+    // Opérateurs autorisés
+    isOperatorOrThrow(ctx, getOperatorsAt(this.operators, ctx.carpool.datetime));
+
+    // De plus de 2 kilomètres, dont l’origine et la destination sont sur le territoire francilien et dont l’origine ou
+    // la destination sont situées hors de la commune de Paris.
+    onDistanceRangeOrThrow(ctx, { min: 2_000 });
+
+    // Exclure les trajets Paris-Paris
+    if (startsAt(ctx, { com: ["75056"] }) && endsAt(ctx, { com: ["75056"] })) {
+      throw new NotEligibleTargetException();
+    }
+
+    // Exclure les trajets qui ne sont pas dans l'aom
+    // Code insee de l'aom IDFM en 2025: 287500078
+    // https://annuaire-entreprises.data.gouv.fr/entreprise/ile-de-france-mobilites-287500078
+    if ((!startsAt(ctx, { aom: ["287500078"] }) || !endsAt(ctx, { aom: ["287500078"] }))) {
+      throw new NotEligibleTargetException();
+    }
+
+    // Inscrits dans le Registre de Preuve de Covoiturage (RPC)
+    // avec un niveau de classe de type C tel que défini par le RPC.
+    isOperatorClassOrThrow(ctx, ["C"]);
+  }
+
+  override processStateless(ctx: StatelessContextInterface): void {
+    this.processExclusion(ctx);
+    super.processStateless(ctx);
+
+    // Calcul de l'incitation par tranches (additif)
+    let amount = 0;
+    for (const { start, fn } of this.slices) {
+      if (onDistanceRange(ctx, { min: start })) {
+        amount += fn(ctx);
+      }
+    }
+
+    ctx.incentive.set(amount);
+  }
+
+  params(): PolicyHandlerParamsInterface {
+    return {
+      tz: "Europe/Paris",
+      slices: this.slices,
+      operators: getOperatorsAt(this.operators),
+      allTimeOperators: Array.from(new Set(this.operators.flatMap((entry) => entry.operators))),
+      limits: { glob: this.max_amount },
+      booster_dates: [...this.boosterDates],
+      extras: {},
+    };
+  }
+
+  describe(): string {
+    return description;
+  }
+};
---
+++ api/src/pdc/services/policy/engine/policies/20251201_Covoit_IDFM_2026.unit.spec.ts
@@ -0,0 +1,264 @@
+import { describe, it } from "@/dev_deps.ts";
+import { v4 as uuidV4 } from "@/lib/uuid/index.ts";
+import { OperatorsEnum } from "../../interfaces/index.ts";
+import { makeProcessHelper } from "../tests/macro.ts";
+import { CovoitIDFM2026 as Handler } from "./20251201_Covoit_IDFM_2026.ts";
+
+const defaultPosition = {
+  arr: "91377",
+  com: "91377",
+  aom: "287500078",
+  epci: "200056232",
+  dep: "91",
+  reg: "11",
+  country: "XXXXX",
+  reseau: 232,
+};
+const defaultLat = 48.72565703413325;
+const defaultLon = 2.261827843187402;
+
+const defaultCarpool = {
+  _id: 1,
+  operator_trip_id: uuidV4(),
+  passenger_identity_key: uuidV4(),
+  driver_identity_key: uuidV4(),
+  operator_uuid: OperatorsEnum.COVOIT_IDFM,
+  operator_class: "C",
+  passenger_is_over_18: true,
+  passenger_has_travel_pass: true,
+  driver_has_travel_pass: true,
+  datetime: new Date("2026-02-01"),
+  seats: 1,
+  distance: 5_000,
+  operator_journey_id: uuidV4(),
+  operator_id: 1,
+  driver_revenue: 2_00,
+  passenger_contribution: 0,
+  start: { ...defaultPosition },
+  end: { ...defaultPosition },
+  start_lat: defaultLat,
+  start_lon: defaultLon,
+  end_lat: defaultLat,
+  end_lon: defaultLon,
+};
+
+const process = makeProcessHelper(defaultCarpool);
+
+describe("Covoit IDFM 2026 - Exclusions", () => {
+  it("unsupported operator", async () =>
+    await process({
+      policy: { handler: Handler.id },
+      carpool: [{ operator_uuid: "not in list" }],
+    }, { incentive: [0] }));
+
+  it("distance below minimum", async () =>
+    await process({
+      policy: { handler: Handler.id },
+      carpool: [{ distance: 1_000 }],
+    }, { incentive: [0] }));
+
+  it("trip Paris-Paris", async () =>
+    await process({
+      policy: { handler: Handler.id },
+      carpool: [
+        {
+          start: { ...defaultPosition, com: "75056" },
+          end: { ...defaultPosition, com: "75056" },
+        },
+      ],
+    }, { incentive: [0] }));
+
+  it("trip outside AOM", async () =>
+    await process({
+      policy: { handler: Handler.id },
+      carpool: [
+        { start: { ...defaultPosition, aom: "not_ok" } },
+      ],
+    }, { incentive: [0] }));
+
+  it("trip outside AOM (end)", async () =>
+    await process({
+      policy: { handler: Handler.id },
+      carpool: [
+        { end: { ...defaultPosition, aom: "not_ok" } },
+      ],
+    }, { incentive: [0] }));
+
+  it("operator class not eligible", async () =>
+    await process({
+      policy: { handler: Handler.id },
+      carpool: [{ operator_class: "A" }],
+    }, { incentive: [0] }));
+
+  it("trip outside of campaign dates", async () =>
+    await process({
+      policy: { handler: Handler.id },
+      carpool: [{ datetime: new Date("2022-09-02") }],
+    }, { incentive: [0] }));
+});
+
+describe("Covoit IDFM 2026 - Incentives", () => {
+  it("first slice", async () =>
+    await process({
+      policy: { handler: Handler.id },
+      carpool: [
+        { operator_trip_id: uuidV4(), distance: 2_000 },
+        { operator_trip_id: uuidV4(), distance: 5_000 },
+        { operator_trip_id: uuidV4(), distance: 10_000 },
+        { operator_trip_id: uuidV4(), distance: 20_000 },
+      ],
+    }, { incentive: [200, 200, 200, 200] }));
+
+  it("second slice", async () =>
+    await process({
+      policy: { handler: Handler.id },
+      carpool: [
+        { operator_trip_id: uuidV4(), distance: 20_000 },
+        { operator_trip_id: uuidV4(), distance: 25_000 },
+        { operator_trip_id: uuidV4(), distance: 30_000 },
+        { operator_trip_id: uuidV4(), distance: 80_000 },
+      ],
+    }, { incentive: [200, 250, 300, 300] }));
+
+  it("outside of second slice", async () =>
+    await process({
+      policy: { handler: Handler.id },
+      carpool: [
+        { distance: 80_000 },
+        { distance: 100_000 },
+      ],
+    }, { incentive: [300, 300] }));
+});
+
+describe("Covoit IDFM 2026 - Driver trip limits", () => {
+  it("should limit driver passenger per trip to 3", async () => {
+    const operator_trip_id = uuidV4(); // same trip for all carpools
+
+    await process({
+      policy: { handler: Handler.id },
+      carpool: [
+        { driver_identity_key: "1", operator_trip_id, passenger_identity_key: "emile" },
+        { driver_identity_key: "1", operator_trip_id, passenger_identity_key: "georgette" },
+        { driver_identity_key: "1", operator_trip_id, passenger_identity_key: "bob" },
+        { driver_identity_key: "1", operator_trip_id, passenger_identity_key: "alice" }, // sorry Alice
+        { driver_identity_key: "1", passenger_identity_key: "jean" }, // different trip, ok
+        { driver_identity_key: "2", operator_trip_id: uuidV4(), passenger_identity_key: "marie", seats: 3 }, // Marie & friends
+        { driver_identity_key: "2", operator_trip_id: uuidV4(), passenger_identity_key: "paul", seats: 7 }, // Paul & friend (exceeds)
+        { driver_identity_key: "2", operator_trip_id: uuidV4(), passenger_identity_key: "marty", seats: 1 }, // Marty alone
+      ],
+    }, { incentive: [200, 200, 200, 0, 200, 600, 600, 0] });
+  });
+});
+
+describe("Covoit IDFM 2026 - Driver daily limits", () => {
+  it("should limit driver daily trips to 4", async () =>
+    await process({
+      policy: { handler: Handler.id },
+      carpool: [
+        { operator_trip_id: uuidV4() },
+        { operator_trip_id: uuidV4() },
+        { operator_trip_id: uuidV4() },
+        { operator_trip_id: uuidV4() },
+        { operator_trip_id: uuidV4() }, /* exceeds limit here */
+      ],
+    }, { incentive: [200, 200, 200, 200, 0] }));
+
+  it("should limit driver daily passengers to 6", async () =>
+    await process({
+      policy: { handler: Handler.id },
+      carpool: [
+        { operator_trip_id: uuidV4(), seats: 2 },
+        { operator_trip_id: uuidV4(), seats: 2 },
+        { operator_trip_id: uuidV4(), seats: 2 },
+        { operator_trip_id: uuidV4(), seats: 2 }, // exceeds limit here
+      ],
+    }, { incentive: [400, 400, 400, 0] }));
+
+  it("should reset limits on different days", async () =>
+    await process({
+      policy: { handler: Handler.id },
+      carpool: [
+        { operator_trip_id: uuidV4(), datetime: new Date("2026-02-01T10:00:00") },
+        { operator_trip_id: uuidV4(), datetime: new Date("2026-02-01T12:00:00") },
+        { operator_trip_id: uuidV4(), datetime: new Date("2026-02-01T14:00:00") },
+        { operator_trip_id: uuidV4(), datetime: new Date("2026-02-01T16:00:00") },
+        { operator_trip_id: uuidV4(), datetime: new Date("2026-02-01T18:00:00") }, // exceeds limit here
+
+        // next day
+        { operator_trip_id: uuidV4(), datetime: new Date("2026-02-02T10:00:00") },
+        { operator_trip_id: uuidV4(), datetime: new Date("2026-02-02T12:00:00") },
+      ],
+    }, { incentive: [200, 200, 200, 200, 0, 200, 200] }));
+});
+
+describe("Covoit IDFM 2026 - Driver monthly limits", () => {
+  it("should limit driver monthly earnings to 200 €", async () => {
+    await process({
+      policy: { handler: Handler.id },
+      carpool: [{ driver_identity_key: "driver_1" }],
+      meta: [{
+        key: "max_amount_restriction.0-driver_1.month.1-2026",
+        value: 199_00n,
+      }, {
+        key: "max_amount_restriction.global.campaign.global",
+        value: 199_00n,
+      }],
+    }, {
+      incentive: [1_00], // only 1€ left to reach the limit
+      meta: [{
+        key: "max_amount_restriction.0-driver_1.month.1-2026",
+        value: 200_00n,
+      }, {
+        key: "max_amount_restriction.global.campaign.global",
+        value: 200_00n,
+      }],
+    });
+  });
+
+  it("should reset limits on next month", async () => {
+    await process({
+      policy: { handler: Handler.id },
+      carpool: [
+        { driver_identity_key: "driver_1", datetime: new Date("2026-02-10") },
+        { driver_identity_key: "driver_1", datetime: new Date("2026-03-10") },
+      ],
+      meta: [{
+        key: "max_amount_restriction.0-driver_1.month.1-2026",
+        value: 199_00n,
+      }, {
+        key: "max_amount_restriction.global.campaign.global",
+        value: 199_00n,
+      }],
+    }, {
+      incentive: [
+        1_00, // only 1€ left to reach the limit
+        2_00, // reset for new month : full incentive
+      ],
+      meta: [{
+        key: "max_amount_restriction.0-driver_1.month.1-2026", // february
+        value: 200_00n,
+      }, {
+        key: "max_amount_restriction.global.campaign.global",
+        value: 202_00n,
+      }, {
+        key: "max_amount_restriction.0-driver_1.month.2-2026", // march
+        value: 2_00n,
+      }],
+    });
+  });
+});
+
+describe("Covoit IDFM 2026 - Campaign global limit", () => {
+  it("should limit campaign total amount to 16M €", async () => {
+    await process({
+      policy: { handler: Handler.id, max_amount: 16_000_00_000n },
+      carpool: [{}, {}, {}],
+      meta: [{
+        key: "max_amount_restriction.global.campaign.global",
+        value: 15_999_99_900n,
+      }],
+    }, {
+      incentive: [100, 0, 0], // only 0.10€ left to reach the limit
+    });
+  });
+});
