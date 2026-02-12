04fa0922ce157fbe86c67d6a36b1ccc3f4f80211
Benjamin Guendouz <bqyben@gmail.com>
2026-02-12 12:31:43 +0100

    fix(analytics): change user's analytics_id for uuid

--- api/src/db/migrations/20260212000000-add-analytics-id-to-users.sql
+++ api/src/db/migrations/20260212000000-add-analytics-id-to-users.sql
@@ -1,12 +1,12 @@
 -- Add analytics_id column (nullable first for backfill)
-ALTER TABLE auth.users ADD COLUMN analytics_id VARCHAR(21) UNIQUE;
+ALTER TABLE auth.users ADD COLUMN analytics_id UUID UNIQUE;
 
 -- Backfill existing users
 UPDATE auth.users
-  SET analytics_id = substr(replace(replace(encode(gen_random_bytes(16), 'base64'), '+', '-'), '/', '_'), 1, 21)
+  SET analytics_id = gen_random_uuid()
   WHERE analytics_id IS NULL;
 
 -- Set NOT NULL and default
 ALTER TABLE auth.users
   ALTER COLUMN analytics_id SET NOT NULL,
-  ALTER COLUMN analytics_id SET DEFAULT substr(replace(replace(encode(gen_random_bytes(16), 'base64'), '+', '-'), '/', '_'), 1, 21);
+  ALTER COLUMN analytics_id SET DEFAULT gen_random_uuid();
