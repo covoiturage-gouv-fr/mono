fe8185539dc70bc91a29136e0626e15d2f56ed4c
Ludovic Delhomme <ludovic.delhomme@datayama.fr>
2026-02-13 18:37:29 +0100

    fix: name part_campaigns

--- sqlmesh/models/3_refined_zone/partenaires/part_campaigns_month.sql
+++ sqlmesh/models/3_refined_zone/partenaires/part_campaigns_month.sql
@@ -10,7 +10,7 @@ MODEL (
 
 WITH last_period AS (
   SELECT COALESCE(MAX(a.year * 100 + a.month), 0)::INT AS value
-  FROM refined_zone.part_campaigns_by_month as a
+  FROM refined_zone.part_campaigns_month as a
 )
 SELECT
   EXTRACT(YEAR FROM start_date)::INT  AS year,
@@ -20,7 +20,7 @@ SELECT
   SUM(journeys)::INT AS journeys,
   SUM(incented_journeys)::INT AS incented_journeys,
   SUM(incentive_amount)::INT  AS incentive_amount
-FROM refined_zone.part_campaigns_by_day
+FROM refined_zone.part_campaigns_day
 WHERE
   (EXTRACT(YEAR FROM start_date)::INT * 100 + EXTRACT(MONTH FROM start_date)::INT)
   >= (SELECT value FROM last_period)
