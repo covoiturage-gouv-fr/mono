dffdaab8be54385aca6e56ba005e6e4ddf715484
Jonathan Fallon <jonathan@scopopop.com>
2026-03-07 00:03:59 +0100

    remove delay dependency

--- api/src/dev_deps.ts
+++ api/src/dev_deps.ts
@@ -12,6 +12,4 @@ export {
 export { assertSpyCall, assertSpyCalls, stub } from "dep:testing-mock";
 export { afterAll, afterEach, beforeAll, beforeEach, describe, it } from "dep:testing-bdd";
 
-// @deno-types="npm:@types/node@^20"
-export { delay } from "https://deno.land/std@0.224.0/async/delay.ts";
 export type { Context } from "node:vm";
