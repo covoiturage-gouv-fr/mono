9848868bdc01a72b5a92df02bb44befdb2a20118
Ludovic Delhomme <ludovic.delhomme@datayama.fr>
2026-03-18 15:14:06 +0100

    fix lookback journeys_latest

--- sqlmesh/models/1_raw_zone/journeys/journeys_latest.sql
+++ sqlmesh/models/1_raw_zone/journeys/journeys_latest.sql
@@ -2,7 +2,7 @@ MODEL (
   name raw_zone.journeys_latest,
   kind INCREMENTAL_BY_TIME_RANGE (
     time_column start_datetime,
-    lookback 2,
+    lookback 3,
     batch_size 30
   ),
   start '2026-01-01 00:00:00+0100',
