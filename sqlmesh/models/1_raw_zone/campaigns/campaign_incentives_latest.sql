d88b619b0c32d385aa76712d435bac7bdba2431a
Jonathan Fallon <jonathan@scopopop.com>
2026-03-19 22:12:05 +0100

    sqlmesh fix campaign_incentives_latest tag

--- sqlmesh/models/1_raw_zone/campaigns/campaign_incentives_latest.sql
+++ sqlmesh/models/1_raw_zone/campaigns/campaign_incentives_latest.sql
@@ -7,7 +7,7 @@ MODEL (
   ),
   start '2026-01-01 00:00:00+0100',
   grain '_id',
-  tags ['archive', 'campaign', 'incentives', 'latest'],
+  tags ['raw', 'campaign', 'incentives', 'latest'],
 );
 
 @campaign_incentives(@start_ts, @end_ts);
