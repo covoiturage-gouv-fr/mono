c0f6bd9cadb64e6cbb4b51967978855820721a24
Jonathan Fallon <jonathan@scopopop.com>
2026-03-21 18:50:33 +0100

    remove duplicated incentives in archive model

--- sqlmesh/macros/campaign_incentives.py
+++ sqlmesh/macros/campaign_incentives.py
@@ -13,19 +13,6 @@ WITH ni AS (
   SELECT
     pi._id,
     c2._id AS carpool_v2_id,
-    c2.operator_id,
-    c2.operator_journey_id
-  FROM policy.incentives pi
-  LEFT JOIN carpool.carpools c1 ON pi.carpool_id = c1._id
-  LEFT JOIN carpool_v2.carpools c2 ON c1.acquisition_id = c2.legacy_id
-  WHERE pi.carpool_id IS NOT NULL
-    AND pi.datetime BETWEEN '{start_ts}' AND '{end_ts}'
-
-  UNION
-
-  SELECT
-    pi._id,
-    c2._id AS carpool_v2_id,
     pi.operator_id,
     pi.operator_journey_id
   FROM policy.incentives pi
@@ -33,9 +20,24 @@ WITH ni AS (
     ON pi.operator_id = c2.operator_id AND pi.operator_journey_id = c2.operator_journey_id
   WHERE pi.operator_id IS NOT NULL AND pi.operator_journey_id IS NOT NULL
     AND pi.datetime BETWEEN '{start_ts}' AND '{end_ts}'
+
+  UNION
+
+  -- fallback to Carpool V1 _id
+  SELECT
+    pi._id,
+    c2._id AS carpool_v2_id,
+    c2.operator_id,
+    c2.operator_journey_id
+  FROM policy.incentives pi
+  LEFT JOIN carpool.carpools c1 ON pi.carpool_id = c1._id
+  LEFT JOIN carpool_v2.carpools c2 ON c1.acquisition_id = c2.legacy_id
+  WHERE pi.carpool_id IS NOT NULL
+    AND (pi.operator_id IS NULL OR pi.operator_journey_id IS NULL)
+    AND pi.datetime BETWEEN '{start_ts}' AND '{end_ts}'
 )
 
-SELECT
+SELECT DISTINCT
   pi._id,
   ni.carpool_v2_id,
   pi.datetime,
