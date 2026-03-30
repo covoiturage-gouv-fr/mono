c22a220a752d812f5e5d5e87b75de0a2262dcff3
Ludovic Delhomme <74348195+Datayama38@users.noreply.github.com>
2026-03-30 18:55:27 +0200

    Sqlmesh archive (#3117)
    * wip restore archive
    
    * wip: add export script duckdb
    
    * wip remove files in utils
    
    * wip remove uploads in utils
    
    * wip verify file exist in s3
    
    * wip restore archive
    
    * wip: add export script duckdb
    
    * wip remove files in utils
    
    * wip remove uploads in utils
    
    * wip verify file exist in s3
    
    * feat: add import from parquet file with duckDB
    
    * feat: add just command

---
+++ sqlmesh/scripts/import_archive.py
@@ -0,0 +1,31 @@
+"""
+Importer les fichiers Parquet depuis S3 vers la zone archive.
+
+Usage:
+    cd sqlmesh && python -m scripts.import_archive          
+"""
+from utils.duckdb import duckdb_client, import_tables
+
+conn = duckdb_client()
+TABLES = [
+  'incentives_2019',
+  'incentives_2020',
+  'incentives_2021',
+  'incentives_2022',
+  'incentives_2023',
+  'incentives_2024',
+  'incentives_2025',
+  'incentives_latest',
+  'journeys_2019',
+  'journeys_2020',
+  'journeys_2021',
+  'journeys_2022',
+  'journeys_2023',
+  'journeys_2024',
+  'journeys_2025',
+  'journeys_latest',
+  'cee_applications',
+]
+
+import_tables(conn, TABLES, schema='archive_zone', folder='exports')
+
