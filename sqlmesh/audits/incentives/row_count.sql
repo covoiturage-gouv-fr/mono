be1a15b01f6cdd555fe4c64f7ffa885ad4fe53d4
Ludovic Delhomme <74348195+Datayama38@users.noreply.github.com>
2026-04-02 15:03:08 +0200

    fix: incentive audit count (#3135)

--- sqlmesh/audits/incentives/row_count.sql
+++ sqlmesh/audits/incentives/row_count.sql
@@ -13,8 +13,8 @@ SELECT 1 AS failure
 WHERE (
   SELECT COUNT(*)
   FROM policy.incentives
-  WHERE datetime pi.datetime >= @start_ts::timestamp - INTERVAL '1 day' 
-  AND pi.datetime < @end_ts::timestamp + INTERVAL '1 day'
+  WHERE datetime >= @start_ts::timestamp - INTERVAL '1 day' 
+  AND datetime < @end_ts::timestamp + INTERVAL '1 day'
 ) != (
   SELECT COUNT(*) FROM @this_model
 );
