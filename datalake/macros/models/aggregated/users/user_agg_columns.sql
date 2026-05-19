883b253e7e889a368bb7ac456e05e6ca7bac73ac
Ludovic Delhomme <ludovic.delhomme@datayama.fr>
2026-05-19 15:11:46 +0200

    fix: operators and users models

---
+++ datalake/macros/models/aggregated/users/user_agg_columns.sql
@@ -0,0 +1,34 @@
+{% macro user_agg_columns() %}
+  COUNT(*) AS carpools,
+  COUNT(DISTINCT operator_trip_id) AS trips,
+  SUM(distance) AS distance,
+  SUM(driver_revenue) AS driver_revenue,
+  SUM(passenger_seats) AS passenger_seats,
+  COUNT(*) FILTER (WHERE passenger_over_18) AS passenger_over_18,
+  SUM(passenger_contribution) AS passenger_contribution,
+  SUM(oi_collectivite) AS oi_collectivite,
+  SUM(oi_operator) AS oi_operator,
+  SUM(oi_other) AS oi_other,
+  COUNT(*) FILTER (WHERE NOT with_incentive) AS no_oi,
+  SUM(oi_amount_collectivite) AS oi_amount_collectivite,
+  SUM(oi_amount_operator) AS oi_amount_operator,
+  SUM(oi_amount_other) AS oi_amount_other,
+  SUM(oi_amount_total) AS oi_amount_total,
+  SUM(
+    jsonb_array_length(
+      COALESCE(campaigns, '[]'::jsonb)
+    )
+  ) AS ci_collectivite,
+  SUM(campaigns_amount_total) AS ci_amount_total,
+  SUM(campaigns_result_total) AS ci_result_total,
+  ARRAY[
+    {% for h in range(24) %}
+      COUNT(*) FILTER (WHERE hour = {{h}}){% if not loop.last %},{% endif %}
+    {% endfor %}
+  ] AS hours_distribution,
+  ARRAY[
+    COUNT(*) FILTER (WHERE operator_class = 'A'),
+    COUNT(*) FILTER (WHERE operator_class = 'B'),
+    COUNT(*) FILTER (WHERE operator_class = 'C')
+  ] AS oc_distribution
+{% endmacro %}
--- datalake/models/aggregated/users/user_od_day.sql
+++ datalake/models/aggregated/users/user_od_day.sql
@@ -2,46 +2,36 @@
   config(
     materialized='incremental',
     incremental_strategy='delete+insert',
-    unique_key=['user_id', 'incremental_date', 'role', 'start_geo_code', 'end_geo_code'],
+    unique_key=['user_id', 'incremental_date', 'role', 'start_code', 'end_code'],
     indexes=[
       { 'columns': ['user_id'] },
       { 'columns': ['incremental_date'] },
-      { 'columns': ['start_geo_code', 'end_geo_code'] }
+      { 'columns': ['start_code', 'end_code'] }
     ],
     tags=['aggregated', 'users', 'od', 'daily']
   )
 }}
 
-WITH carpools AS (
-  SELECT
-    driver_key AS user_id,
-    start_datetime_tz::date AS carpool_date,
-    'driver' AS role,
-    start_geo_code,
-    end_geo_code
-  FROM {{ ref('carpools') }}
-  WHERE {{ time_filter('start_datetime_tz', 'incremental_date', type='timestamp', default_start="'2020-01-01 00:00:00'", lookback_nb=3) }}
-    AND driver_key IS NOT NULL
-    AND valid_acquisition_status = true
+WITH filtered_carpools AS (
+  {{ filtered_carpools('arr', lookback_nb=3, lookback_unit='day', with_new_users=false) }}
+),
+
+carpools_by_role AS (
+  SELECT driver_key AS user_id, 'driver'::text AS role, fc.*
+  FROM filtered_carpools fc
+  WHERE driver_key IS NOT NULL
   UNION ALL
-  SELECT
-    passenger_key AS user_id,
-    start_datetime_tz::date AS carpool_date,
-    'passenger' AS role,
-    start_geo_code,
-    end_geo_code
-  FROM {{ ref('carpools') }}
-  WHERE {{ time_filter('start_datetime_tz', 'incremental_date', type='timestamp', default_start="'2020-01-01 00:00:00'", lookback_nb=3) }}
-    AND passenger_key IS NOT NULL
-    AND valid_acquisition_status = true
+  SELECT passenger_key AS user_id, 'passenger'::text AS role, fc.*
+  FROM filtered_carpools fc
+  WHERE passenger_key IS NOT NULL
 )
 
 SELECT
   user_id,
-  carpool_date AS incremental_date,
   role,
-  start_geo_code,
-  end_geo_code,
-  COUNT(*) AS carpools
-FROM carpools
+  carpool_datetime::date AS incremental_date,
+  start_code,
+  end_code,
+  {{ user_agg_columns() }}
+FROM carpools_by_role
 GROUP BY 1, 2, 3, 4, 5
