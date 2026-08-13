83efff87cb5219921dff6cd247014fd7a9f54429
Ludovic Delhomme <ludovic.delhomme@datayama.fr>
2026-08-13 15:19:54 +0200

    fix:no incentive graph

--- app-observatory/src/helpers/lists.ts
+++ app-observatory/src/helpers/lists.ts
@@ -63,5 +63,5 @@ export const graphList = [
   { id: 1, name: "Evolution du nombre de trajets" },
   { id: 2, name: "Evolution de la distance moyenne des trajets (en km)" },
   { id: 3, name: "Evolution du taux d'occupation" },
-  { id: 4, name: "Evolution du nombre de véhicules partagés" },
+  //{ id: 4, name: "Evolution du nombre de véhicules partagés" },
 ];
