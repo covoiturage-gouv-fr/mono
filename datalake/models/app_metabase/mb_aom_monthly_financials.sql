a430c94b7a7d335ae57bc2b8f888cf6071dbe3bb
Thomas Grammatico <thomas@grammatico.me>
2026-09-15 11:45:57 +0200

    fix: linting

--- datalake/models/app_metabase/mb_aom_monthly_financials.sql
+++ datalake/models/app_metabase/mb_aom_monthly_financials.sql
@@ -66,10 +66,17 @@ WITH monthly AS (
 SELECT
   perim,
   code,
-  to_char(incremental_date, 'YYYY-MM') AS date,
   carpools,
   intra_carpools,
   inter_carpools,
+  oi_amount_collectivite_self,
+  oi_amount_collectivite_other,
+  ci_amount_total_self,
+  ci_amount_total_other,
+  ci_result_total_self,
+  ci_result_total_other,
+  to_char(incremental_date, 'YYYY-MM')
+    AS date,
   round(driver_revenue::numeric / nullif(carpools, 0), 2)
     AS avg_driver_revenue,
   round(driver_revenue_intra::numeric / nullif(intra_carpools, 0), 2)
@@ -100,17 +107,12 @@ SELECT
     AS avg_incentive_operator_intra,
   round(oi_amount_operator_inter / nullif(inter_carpools, 0), 2)
     AS avg_incentive_operator_inter,
-  oi_amount_collectivite_self,
-  oi_amount_collectivite_other,
-  ci_amount_total_self,
-  ci_amount_total_other,
-  ci_result_total_self,
-  ci_result_total_other,
   oi_amount_collectivite_self - ci_result_total_self
     AS diff_op_rpc,
   round(
     100.0 * (oi_amount_collectivite_self - ci_result_total_self)
     / nullif(ci_result_total_self, 0), 1
-  ) AS diff_op_rpc_pct
+  )
+    AS diff_op_rpc_pct
 FROM monthly
 ORDER BY date, perim, code
